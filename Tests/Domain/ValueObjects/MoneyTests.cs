e6b26b1847c84b70caa5f75caf931a35c0117442
Linkgold <linkgold@gmail.com>
2026-08-04 21:20:59 +0200

    Update currency assertion in MoneyTests

--- Tests/Domain/ValueObjects/MoneyTests.cs
+++ Tests/Domain/ValueObjects/MoneyTests.cs
@@ -12,7 +12,7 @@ namespace Tests.Domain.ValueObjects
 
             // Assert
             Assert.Equal(100.50m, money.Value);
-            Assert.Equal("MAL!", money.Currency);
+            Assert.Equal("IUR", money.Currency);
         }
 
         [Fact]
